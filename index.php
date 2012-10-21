65a39281c0cf2c3eb49263d9c10ef95387f01435
snwfog <donchoa@gmail.com>
2012-10-21 04:02:52 -0400

    Add initial files and scaffoldings

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>COMP353 - Databases</title>
+</head>
+<body>
+<p>Welcome to COMP 353 - Databases website</p>
+<p>This website is setup by Charles Yang through shell on September 26, 2012.</p>
+<p>You are viewing this page on <? echo date("D M d, Y H:i:s", time()) ?>
+
+<h1>Data Entry Form LOL</h1>
+
+<ul>
+<li><a href="member.php">Members</a>
+<li><a href="listing.php">Listings</a>
+<li><a href="credit_card.php">Credit Cards</a></li>
+<li><a href="card_holder.php">Card Holders</a></li>
+<ul>
+
+</body>
+</html>
+
