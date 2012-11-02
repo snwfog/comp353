6ca307aea321a511571f6d8589b4d380c44ac719
snwfog <donchoa@gmail.com>
2012-11-01 20:39:45 -0400

    Setup test on old macbook pro

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 </head>
 <body>
 <p>I JUST ADDED GIT HOOK AND ITS WORKING!!!</p>
+<p>Just setup on old macbook pro</p>
 <p>Welcome to COMP 353 - Databases website</p>
 <p>This website is setup by Charles Yang through shell on September 26, 2012.</p>
 <p>You are viewing this page on <? echo date("D M d, Y H:i:s", time()) ?>
