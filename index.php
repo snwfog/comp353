d3f1c7ffa7c4ac25e305f928c2790881632b3fb6
snwfog <donchoa@gmail.com>
2012-11-01 20:10:55 -0400

    Add git hook

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <title>COMP353 - Databases</title>
 </head>
 <body>
+<p>I JUST ADDED GIT HOOK AND ITS WORKING!!!</p>
 <p>Welcome to COMP 353 - Databases website</p>
 <p>This website is setup by Charles Yang through shell on September 26, 2012.</p>
 <p>You are viewing this page on <? echo date("D M d, Y H:i:s", time()) ?>
