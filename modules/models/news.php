fe726fd5fe9de89a399cb29e13fac899aefd3598
shehaaz <shehaaz@gmail.com>
2012-11-10 13:14:16 -0500

    didn't change anything.news.php is the same

--- modules/models/news.php
+++ modules/models/news.php
@@ -2,55 +2,55 @@
 
 class News_Model
 {
-    private $articles = array
-    (
-        'new' => array
-        (
-            'title' => 'New Website',
-            'content' => 'Welcome to the site! We are glad to have you here'
-        ),
+	private $articles = array
+	(
+			'new' => array
+			(
+					'title' => 'New Website',
+					'content' => 'Welcome to the site! We are glad to have you here'
+			),
 
-        'mvc' => array
-        (
-            'title' => 'PHP MVC Frameworks',
-            'content' => 'It really is very easy, take it from us'
-        ),
+			'mvc' => array
+			(
+					'title' => 'PHP MVC Frameworks',
+					'content' => 'It really is very easy, take it from us'
+			),
 
-        'test' => array
-        (
-            'title' => 'Testing',
-            'content' => 'Testing content here bla bla bla'
-        )
+			'test' => array
+			(
+					'title' => 'Testing',
+					'content' => 'Testing content here bla bla bla'
+			)
 
-    );
+	);
 
-    public function __construct() {
+	public function __construct() {
+		echo "this is constrcuted";
+		$dbconfig = array();
+		$dbconfig['host'] = 'localhost';
+		$dbconfig['user'] = 'root';
+		$dbconfig['pass'] = 'root';
+		$dbconfig['table'] = 'comp353';
 
-        $dbconfig = array();
-        $dbconfig['host'] = 'localhost';
-        $dbconfig['user'] = 'root';
-        $dbconfig['pass'] = 'root';
-        $dbconfig['table'] = 'comp353';
 
+		require_once('libs/mysqli.class.php');
+		// Then simply connect to your DB this way:
+		$db = new DB($dbconfig);
 
-        require_once('libs/mysqli.class.php');
-        // Then simply connect to your DB this way:
-        $db = new DB($dbconfig);
+		// Run a Query:
+		$db->query('SELECT * FROM states');
 
-        // Run a Query:
-        $db->query('SELECT * FROM states');
+		// Get an array of items:
+		$result = $db->get();
 
-        // Get an array of items:
-        $result = $db->get();
+		print "<pre>";
+		print_r($result);
+		print "</pre>";
+	}
 
-        print "<pre>";
-        print_r($result);
-        print "</pre>";
-    }
-
-    public function getArticles($articleName)
-    {
-        $article = $this->articles[$articleName];
-        return $article;
-    }
+	public function getArticles($articleName)
+	{
+		$article = $this->articles[$articleName];
+		return $article;
+	}
 }
