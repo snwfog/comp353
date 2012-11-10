195950157035aeac64fc623697aa4bbf2422163c
shehaaz <shehaaz@gmail.com>
2012-11-10 17:14:57 -0500

    VIEW Template using TWIG v.1

--- modules/models/news.php
+++ modules/models/news.php
@@ -25,27 +25,27 @@ class News_Model
 	);
 
 	public function __construct() {
-		echo "this is constrcuted";
-		$dbconfig = array();
-		$dbconfig['host'] = 'localhost';
-		$dbconfig['user'] = 'root';
-		$dbconfig['pass'] = 'root';
-		$dbconfig['table'] = 'comp353';
+		// echo "this is constrcuted";
+		// $dbconfig = array();
+		// $dbconfig['host'] = 'localhost';
+		// $dbconfig['user'] = 'root';
+		// $dbconfig['pass'] = 'root';
+		// $dbconfig['table'] = 'comp353';
 
 
-		require_once('libs/mysqli.class.php');
-		// Then simply connect to your DB this way:
-		$db = new DB($dbconfig);
+		// require_once('libs/mysqli.class.php');
+		// // Then simply connect to your DB this way:
+		// $db = new DB($dbconfig);
 
-		// Run a Query:
-		$db->query('SELECT * FROM states');
+		// // Run a Query:
+		// $db->query('SELECT * FROM states');
 
-		// Get an array of items:
-		$result = $db->get();
+		// // Get an array of items:
+		// $result = $db->get();
 
-		print "<pre>";
-		print_r($result);
-		print "</pre>";
+		// print "<pre>";
+		// print_r($result);
+		// print "</pre>";
 	}
 
 	public function getArticles($articleName)
