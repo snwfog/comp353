a1f6cbe6d75f3e294d066ea7e47c2f0007968e9b
nimonikMike <viet_mike.pham@hotmail.com>
2012-11-13 14:40:48 -0500

    nothing much

--- modules/controllers/login.php
+++ modules/controllers/login.php
@@ -21,38 +21,4 @@ class Login_Controller extends Controller implements IRedirectable
             $this->display('login.twig');
         }
     }
-
-    //private $params = array();
-//    public function __construct(array $data){
-//        $this->params = $data;
-//        $class_methods = get_class_methods($this);
-//        if(isset($this->params["controller"])){
-//            $method = trim(urldecode($this->params["controller"]), "\"");
-//            if (in_array($method, $class_methods)){
-//                $this::$method();
-//            }else{
-//                echo "no such methods";
-//            }
-//        }
-//    }
-//
-//    public function main_login(){
-//      $this->display("main_login.html",$this->params);
-//    }
-//
-//    public function login_user(){
-//       $loginModel = new Login_Model;
-//       $loginModel->get_user();
-//        //notfinished
-//    }
-//
-//    public function register(){
-//        if (count($_POST) !== 0){
-//          $loginModel = new Login_Model;
-//
-//        }else{
-//          $this->display("registration_form.html",$this->params);
-//        }
-//    }
-
 }
--- modules/models/registration.php
+++ modules/models/registration.php
@@ -1,11 +1,13 @@
 <?php
 
-class Login_Model{
+class Registration_Model{
     private $db;
     public function __construct() {
         $this->db = Database::getInstance();
     }
 
     public function get_user(){
+    }
+    public function create_user(){
     }
 }
