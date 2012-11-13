b349249255ad16ee036c88b92fcec1eabcd78194
nimonikMike <viet_mike.pham@hotmail.com>
2012-11-13 17:24:17 -0500

    more error messsages

--- modules/controllers/registration.php
+++ modules/controllers/registration.php
@@ -16,9 +16,17 @@ class Registration_Controller extends Controller implements IRedirectable{
     public function check_form($form){
       $registrationModel = new Registration_Model();
 
+      if($_POST["password1"] == "" && $_POST["password2"] == ""){
+        array_push($this->data["errors"], "Passwords is empty!");
+      }elseif($_POST["password1"] !== $_Post["password2"]){
+        array_push($this->data["errors"], "Confirm password is different!");
+      }
       if($registrationModel->exist_attribute("username",$_POST["username"])){
         array_push($this->data["errors"], "Username Exist!");
-        $this->display("registration.twig", $this->data);
+      }
+
+      if(count($this->data["errors"])>0){
+            $this->display("registration.twig", $this->data);
       }else{
       }
     }
