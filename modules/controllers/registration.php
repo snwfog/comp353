a0f56faf1e63a468beed8eab0c4389011347fb79
nimonikMike <viet_mike.pham@hotmail.com>
2012-11-13 15:59:23 -0500

    just committing to pull

--- modules/controllers/registration.php
+++ modules/controllers/registration.php
@@ -1,47 +1,11 @@
 <?php
-class Registration_Controller extends Controller implements IRedirectable
-{
-    public function __construct(array $args)
-    {
-        //Check if there are form data
-        if (isset($_POST[]){
-       `}
-        else{
-            $this->display("registration_form.html",$this->params);
-        }
+class Registration_Controller extends Controller implements IRedirectable{
+    public function __construct(array $args){
+      if(isset($_POST["registration_form"])){
+        
+      }else{
+          $this->display("registration.twig");
+      }
     }
-
-//private $params = array();
-//    public function __construct(array $data){
-//        $this->params = $data;
-//        $class_methods = get_class_methods($this);
-//        if(isset($this->params["controller"])){
-//            $method = trim(urldecode($this->params["controller"]), "\"");
-//            if (in_array($method, $class_methods)){
-//                $this::$method();
-//            }else{
-//                echo "no such methods";
-//            }
-//        }
-//    }
-//
-//    public function main_login(){
-//      $this->display("main_login.html",$this->params);
-//    }
-//
-//    public function login_user(){
-//       $loginModel = new Login_Model;
-//       $loginModel->get_user();
-//        //notfinished
-//    }
-//
-//    public function register(){
-//        if (count($_POST) !== 0){
-//          $loginModel = new Login_Model;
-//
-//        }else{
-//
-//        }
-//    }
-
 }
+?>
--- modules/models/registrations.php
+++ modules/models/registrations.php
@@ -2,5 +2,17 @@
 
 class Registration_Model
 {
+    private $db;
+    public function __construct() {
+        $this->db = Database::getInstance();
+    }
+
+    public function create_user($username, $password, $password2, $address_id, $visitor_id, $avatar_url){
+      $this->
+    }
+
+    public function get_attribute($attribute){
+    }
 
 }
+?>
