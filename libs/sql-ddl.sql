dd7489bde8a12049117903da6f8dfc40df21ef01
snwfog <donchoa@gmail.com>
2012-11-13 14:39:37 -0500

    Add new DDL SQL

---
+++ libs/sql-ddl.sql
@@ -0,0 +1,478 @@
+﻿# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.25)
+# Database: comp353
+# Generation Time: 2012-11-13 19:18:26 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table addresses
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `addresses`;
+
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `city` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `province` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `country` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `postal_code` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='http://stackoverflow.com/questions/217945/can-i-have-multipl';
+
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+
+INSERT INTO `addresses` (`id`, `address`, `city`, `province`, `country`, `postal_code`)
+VALUES
+	(1,'123 Fake Street','Montreal','Quebec','Canada','H0H0H0');
+
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table admins
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `admins`;
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `member_id_UNIQUE` (`member_id`),
+  KEY `id_idx` (`member_id`),
+  KEY `fk_admin_members_idx` (`member_id`),
+  CONSTRAINT `fk_admin_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table bids
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bids`;
+
+CREATE TABLE `bids` (
+  `member_id` int(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` text,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `id_idx` (`member_id`),
+  KEY `fk_bids_offers_idx` (`offer_id`),
+  KEY `fk_bids_types_idx` (`type_id`),
+  CONSTRAINT `fk_bids_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bids_offers` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bids_types` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table categories
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categories_types` (`type_id`),
+  CONSTRAINT `fk_categories_types` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` text NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `transaction_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_comments_transactions_idx` (`transaction_id`),
+  KEY `fk_comments_members_idx` (`member_id`),
+  CONSTRAINT `fk_comments_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_transactions` FOREIGN KEY (`transaction_id`) REFERENCES `transacts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table credit_card_types
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `credit_card_types`;
+
+CREATE TABLE `credit_card_types` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table credit_cards
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `credit_cards`;
+
+CREATE TABLE `credit_cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  `credit_card_type_id` int(11) NOT NULL,
+  `number` char(16) NOT NULL DEFAULT '',
+  `expire` char(4) NOT NULL,
+  `verification_code` char(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `number_UNIQUE` (`number`),
+  UNIQUE KEY `member_id_UNIQUE` (`member_id`),
+  KEY `id_idx` (`member_id`),
+  KEY `fk_credit_cards_credit_card_types` (`credit_card_type_id`),
+  CONSTRAINT `fk_credit_cards_credit_card_types` FOREIGN KEY (`credit_card_type_id`) REFERENCES `credit_card_types` (`id`),
+  CONSTRAINT `fk_credit_cards_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table domains
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `domains`;
+
+CREATE TABLE `domains` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table emails
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `emails`;
+
+CREATE TABLE `emails` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `domain_id` int(11) NOT NULL,
+  `top_level_domain_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_emails_domains` (`domain_id`),
+  KEY `fk_emails_top_level_domains` (`top_level_domain_id`),
+  CONSTRAINT `fk_emails_domains` FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`),
+  CONSTRAINT `fk_emails_top_level_domains` FOREIGN KEY (`top_level_domain_id`) REFERENCES `top_level_domains` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table member_reserves
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `member_reserves`;
+
+CREATE TABLE `member_reserves` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  `reserve_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_member_reserves_members_idx` (`member_id`),
+  KEY `fk_member_reserves_offers_idx` (`offer_id`),
+  KEY `fk_member_reserves_members_idx1` (`member_id`),
+  KEY `fk_member_reserves_offers_idx1` (`offer_id`),
+  CONSTRAINT `fk_member_reserves_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_member_reserves_offers` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table members
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `members`;
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(256) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `password` varchar(256) NOT NULL DEFAULT '',
+  `address_id` int(11) NOT NULL,
+  `visitor_id` int(11) NOT NULL,
+  `avatar_url` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `visitor_id_UNIQUE` (`visitor_id`),
+  KEY `id_idx` (`address_id`),
+  KEY `fk_members_visitors_idx` (`visitor_id`),
+  CONSTRAINT `fk_members_addresses` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`),
+  CONSTRAINT `fk_members_visitors` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+
+INSERT INTO `members` (`id`, `username`, `password`, `address_id`, `visitor_id`, `avatar_url`)
+VALUES
+	(2,'snw','1234',1,1,NULL);
+
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table offers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `offers`;
+
+CREATE TABLE `offers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `image_url` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_offers_categories` (`category_id`),
+  CONSTRAINT `fk_offers_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table posts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `member_id` int(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  KEY `fk_posts_members_idx` (`member_id`),
+  KEY `fk_posts_offers_idx` (`offer_id`),
+  KEY `fk_posts_members_idx1` (`member_id`),
+  KEY `fk_posts_offers_idx1` (`offer_id`),
+  CONSTRAINT `fk_posts_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_posts_offers` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table ratings
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `ratings`;
+
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rate` int(1) NOT NULL,
+  `transaction_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `id_idx` (`transaction_id`),
+  KEY `id_idx1` (`member_id`),
+  KEY `fk_ratings__idx` (`transaction_id`),
+  KEY `fk_ratings_members_idx` (`member_id`),
+  CONSTRAINT `fk_ratings_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ratings_transactions` FOREIGN KEY (`transaction_id`) REFERENCES `transacts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table sessions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  `session` varchar(256) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `member_id_UNIQUE` (`member_id`),
+  KEY `fk_sessions_members_idx` (`member_id`),
+  CONSTRAINT `fk_sessions_members` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table test_sessions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `test_sessions`;
+
+CREATE TABLE `test_sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `session` varchar(256) NOT NULL DEFAULT '',
+  `expire` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `test_sessions` WRITE;
+/*!40000 ALTER TABLE `test_sessions` DISABLE KEYS */;
+
+INSERT INTO `test_sessions` (`id`, `username`, `password`, `session`, `expire`)
+VALUES
+	(1,'asdfhakjshdf','aksdjfhajksdhf','',0),
+	(6,'not escape','$&W%*&$*&#*($','123123',0),
+	(7,'asdfasdf','asdfasdf','123123',0),
+	(8,'asdfasdf','sdfsdf','123123',1),
+	(9,'asdfcv','fdfdf','123123',1),
+	(10,'asdfas','asdfasdf','123123',0),
+	(11,'sdfdfs','sdfsdfsdf','123123',0),
+	(12,'asdfasdf','asdfdf','123123',0),
+	(13,'asdfsdf','dfsdfsdf','123123',0),
+	(14,'asdfasdf','sdfsdf','123123',0),
+	(15,'asdfasdf','asdfdf','123123',0),
+	(16,'asdf','asdfdf','123123',0),
+	(17,'asdfasdf','asdfsdf','123123',0),
+	(18,'asdfasdf','asdfsdf','123123',0),
+	(19,'asdf','asdf','123123',0),
+	(20,'snw','1234','123123',0),
+	(21,'snw','efasdfasdf','123123',0),
+	(22,'snw','123123123','123123',4),
+	(23,'asdfasdf','asdfasdf','123123',0),
+	(24,'snw','asdfasdfasdf','123123',0),
+	(25,'snw','asdfasdfasdf','123123',0),
+	(26,'asdf','asdf','123123',0),
+	(27,'snw','asdfasdf','123123',0),
+	(28,'snw','asdfasdfasdf','123123',0),
+	(29,'charles','asd','123123',0);
+
+/*!40000 ALTER TABLE `test_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table top_level_domains
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `top_level_domains`;
+
+CREATE TABLE `top_level_domains` (
+  `id` int(11) NOT NULL,
+  `top_level` varchar(8) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `top_level` (`top_level`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table transacts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `transacts`;
+
+CREATE TABLE `transacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `offer_id` int(11) NOT NULL,
+  `buyer_id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `transact_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `offer_id_UNIQUE` (`offer_id`),
+  KEY `id_idx` (`offer_id`),
+  KEY `id_idx1` (`buyer_id`,`seller_id`),
+  KEY `fk_transactions_offers_idx` (`offer_id`),
+  KEY `fk_transactions_members_idx` (`buyer_id`),
+  KEY `fk_transactions_members_idx1` (`seller_id`),
+  KEY `fk_transactions_members_idx2` (`seller_id`),
+  CONSTRAINT `fk_transactions_members` FOREIGN KEY (`buyer_id`) REFERENCES `members` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transactions_offers` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table types
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `types`;
+
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table visitor_reserves
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `visitor_reserves`;
+
+CREATE TABLE `visitor_reserves` (
+  `visitor_id` int(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  `reserve_time` datetime NOT NULL,
+  KEY `fk_visitor_reserves_visitors_idx` (`visitor_id`),
+  KEY `fk_visitor_reserves_offers` (`offer_id`),
+  CONSTRAINT `fk_visitor_reserves_offers` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_visitor_reserves_visitors` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table visitors
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `visitors`;
+
+CREATE TABLE `visitors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(64) NOT NULL DEFAULT '',
+  `last_name` varchar(64) NOT NULL DEFAULT '',
+  `phone_number` varchar(32) NOT NULL DEFAULT '',
+  `join_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `visitors_id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `visitors` WRITE;
+/*!40000 ALTER TABLE `visitors` DISABLE KEYS */;
+
+INSERT INTO `visitors` (`id`, `first_name`, `last_name`, `phone_number`, `join_date`)
+VALUES
+	(1,'Charles','Yang','5148826452','0000-00-00');
+
+/*!40000 ALTER TABLE `visitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ modules/controllers/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+class Registration_Controller extends Controller
+{
+    public function __construct()
+    {
+        $this->display('registration.twig');
+    }
+}
