6fa7a5d6c5f312c1c49e3b36098ee22b4975a605
Wais Khedri <khedriwais@hotmail.com>
2012-11-13 03:49:32 -0500

    add file location for uploaded file

--- modules/models/upload.php
+++ modules/models/upload.php
@@ -1,4 +1,23 @@
 <?php
+$name = $_FILES['file']['name'];
+$tmp_name = $_FILES['file']['tmp_name'];
+
+if (isset($name))
+{
+	if (!empty($name))
+	{
+		$location = '/assets/img/web/';
+		if(move_uploaded_file($tmp_name, $location.$name))
+		{
+		   echo 'Uploaded';
+		}
+	}
+	else 
+	{	 
+		echo 'Please choose a file.';
+	}
+}
+
 $error = $_FILES['file']['error'];
 
 ?>
