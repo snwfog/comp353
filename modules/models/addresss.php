d6de0b8ffdd3c034c7de2d7f49b58ec61844bcb4
nimonikMike <viet_mike.pham@hotmail.com>
2012-11-13 23:07:33 -0500

    addresss adapts to abstract

--- modules/models/addresss.php
+++ modules/models/addresss.php
@@ -1,24 +1,23 @@
 <?php
 
-class Address_Model
+class Address_Model extends Model
 {
-    private $db;
+    protected $db;
     public function __construct() {
         $this->db = Database::getInstance();
     }
 
 
     public function get_attribute($attribute){
-      $this->db->query("Select ".$attribute." FROM addresses");
-      $result = $this->db->get();
+      $result = $this->get("Select ".$attribute." FROM addresses");
       return  $result;
     }
 
     public function exist_attribute($attribute, $value){
       $value = "\"".$value."\"";
-      $this->db->query("Select ".$attribute." FROM addresses WHERE ".$attribute."=".$value);
+      $result = $this->get("Select ".$attribute." FROM addresses WHERE ".$attribute."=".$value);
 
-      if(count($this->db->get()) > 0){
+      if(count($result) > 0){
         return TRUE;
       }else{
         return FALSE;
@@ -32,27 +31,26 @@ class Address_Model
       $country = "\"".$country."\"";
       $postal_code = "\"".$postal_code."\"";
       $this->db->setFetchMode(2); //This may cause error if db class is singleton
-      $this->db->query("Select *
+      $result = $this->get("Select *
                         FROM addresses 
                         WHERE address=".$address."and city=".$city."and postal_code=".$postal_code
                        );
-      $result = $this->db->get();
       if(count($result) == 1){//no two member can have same address!
         array_push($registration_controller->data["errors"], "Address Already Exist!");
         $registration_controller->display("registration.twig", $registration_controller->data);
       }elseif(count($result) == 0){//no duplicate
         $insert = array($address, $city, $province, $country, $postal_code);
         $insert = implode(",", $insert);
-        $new = $this->db->query("INSERT INTO addresses (address, city, province, country, postal_code)
+        $new = $this->set("INSERT INTO addresses (address, city, province, country, postal_code)
                                  VALUES(".$insert.");"
                                 );
         $new = $this->db->id();
-        $new = $this->db->query("Select *
+        $new = $this->get("Select *
                                 FROM addresses
                                 WHERE id=".$new
                                );
-        $new = $this->db->get();
         return $new;
+        //todo: database error handling!
       }
 
     }
