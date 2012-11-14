cebfefc62617a5fffb2d53e8fbee568e11419dbd
Wais Khedri <khedriwais@hotmail.com>
2012-11-14 17:42:58 -0500

    update post_offers

--- modules/models/post_offers.php
+++ modules/models/post_offers.php
@@ -1,5 +1,18 @@
 <?
+if (isset($_POST['description']) && issset($_POST['price']))
+{	
+	$description=$_POST['description'];
+	$price=$_POST['price'];
 
+	if(!empty($description) && !empty($price))
+	{
+			//fill in 
+	}else
+	{
+		echo 'All fields are required.';
+	}
+	
+}	
 ?>
 
 <form action = "post.php" method="POST">
@@ -7,4 +20,5 @@
 	<textarea name="description" id="description "rows="6" cols="30"></textarea><br><br>
 	Price: <input type="text" name="price" id="price"><br><br>
 	<input type="submit" value="Submit">
+	
 </form>
