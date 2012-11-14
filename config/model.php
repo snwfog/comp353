66cc55e3b3ec0789baa497cafd364e0d72dfa051
snwfog <donchoa@gmail.com>
2012-11-14 15:01:34 -0500

    Stream lined database.php

--- config/model.php
+++ config/model.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Model abstract class
+ */
 abstract class Model
 {
     protected $db;
@@ -9,47 +12,88 @@ abstract class Model
         $this->db = Database::getInstance();
     }
 
-
     /**
      * Helper function to compare one single field and return an unique field.
-     * SELECT $select FROM $table WHERE $what = $where.
+     * <pre>SELECT $select FROM $table WHERE $where = $what</pre>
+     *
      */
-    protected function getAll($where, $what, $select, $table)
+    protected function getAll($select, $where, $what, $table)
     {
         if (is_array($select))
 		{
 			$select_stmt = implode(', ', $select);
 			$select = $select_stmt;
 		}
-		
-		if (is_array($what) && is_array($where)
-				&& count($what) == count($where))
-		{
-			$where_stmt = "$what[0] = $where[0]";
 
-			for ($i = 1; $i < count($what); $i++)
+		if (is_array($where) && is_array($what)
+				&& count($where) == count($what))
+		{
+			$where_stmt = "$where[0] = $what[0]";
+
+			for ($i = 1; $i < count($where); $i++)
 			{
 				$where_stmt .= " AND ";
-				$where_stmt .= "$what[$i] = '$where[$i]'";
+				$where_stmt .= "$where[$i] = '$what[$i]'";
 			}
 		}
-		else
+		else if (isset($where) && isset($what))
 		{
-			$where_stmt = "$what[$i] = '$where[$i]'";
+			$where_stmt = "$where = '$what'";
 		}
 
-		$query = "SELECT $select FROM $table
-				  WHERE $where_stmt";
 
-        $myqli_raw = $this->db->query($query);
-        $result = $this->db->selectField();
+        $query = "SELECT $select FROM $table";
+        if (isset($where_stmt))
+            $query .= " WHERE $where_stmt";
 
-        return empty($result) ? FALSE : $result;
+        $mysqli_result = $this->db->query($query);
+        $result = $this->db->fetch();
+        $this->db->free();
+
+        return empty($result) ? NULL : $result;
     }
 
-	protected function getUnique($where, $what, $select, $table)
+    /**
+     * Get a single row of entry assuming that the record is unique.
+     */
+    protected function getUnique($select, $where, $what, $table)
 	{
-        $result = $this->getAll($where, $what, $select, $table);
-		return empty($result[0]) ? FALSE : $result[0];
+        $result = $this->getAll($select, $where, $what, $table);
+		return empty($result[0]) ? NULL : $result[0];
 	}
+
+    /**
+     * Get an array of all the attribute in a single column format.
+     */
+    protected function getColumn($select, $where, $what, $table)
+    {
+        $result = $this->getAll($select, $where, $what, $table);
+        $data = array();
+
+        if (isset($result))
+        {
+            if (is_array($select))
+            {
+                foreach ($select as $key)
+                {
+                    $data[$key] = array();
+                }
+
+                foreach ($result as $row)
+                {
+                    foreach ($select as $key)
+                    {
+                        $data[$key][] = $row[$key];
+                    }
+                }
+            }
+            else
+            {
+                foreach ($result as $row)
+                    $data[$select][] = $row[$select];
+            }
+        }
+
+        return empty($data) ? NULL : $data;
+    }
 }
--- modules/controllers/member.php
+++ modules/controllers/member.php
@@ -10,28 +10,28 @@ class Member_Controller extends Controller implements IRedirectable
         $e = "donchoa@gmail.com";
 
         $email = new Email_Model;
-        $email_id = $email->getIdByEmail($e);
-        if (empty($email_id))
-        {
-            $email_id = $email->setEmailAndGetId($e, 2);
-            if (!$email_id)
-                echo "Your email has serious problem";
-
-        }
-
-        // Try to restitch the email
-        $restitched = $email->getEmailById($email_id);
-
-        $avatar = new Avatar_Model;
-        $avatar_url = $avatar->getGravatarURL($restitched);
-
-        $data = array
-        (
-            "username" => "Charles",
-            "avatar_url" => $avatar_url
-        );
-
-        $this->display("member.twig", $data);
+//        $email_id = $email->getIdByEmail($e);
+//        if (empty($email_id))
+//        {
+//            $email_id = $email->setEmailAndGetId($e, 2);
+//            if (!$email_id)
+//                echo "Your email has serious problem";
+//
+//        }
+//
+//        // Try to restitch the email
+//        $restitched = $email->getEmailById($email_id);
+//
+//        $avatar = new Avatar_Model;
+//        $avatar_url = $avatar->getGravatarURL($restitched);
+//
+//        $data = array
+//        (
+//            "username" => "Charles",
+//            "avatar_url" => $avatar_url
+//        );
+//
+//        $this->display("member.twig", $data);
     }
 
 }
