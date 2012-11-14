a831b9b27e4d8decf6972690a58c688c469687eb
Wais Khedri <khedriwais@hotmail.com>
2012-11-14 18:03:41 -0500

    added upload image to post offers

--- modules/models/post_offers.php
+++ modules/models/post_offers.php
@@ -1,4 +1,6 @@
 <?
+include 'upload.php';
+
 if (isset($_POST['description']) && issset($_POST['price']))
 {	
 	$description=$_POST['description'];
@@ -19,6 +21,9 @@ if (isset($_POST['description']) && issset($_POST['price']))
 	Description:<br> 
 	<textarea name="description" id="description "rows="6" cols="30"></textarea><br><br>
 	Price: <input type="text" name="price" id="price"><br><br>
+	Image: <form action= "upload.php" method="POST" enctype="multipart/form-data">
+			<input type="file" name="file"><br><br>
+			</form>
 	<input type="submit" value="Submit">
 	
 </form>
