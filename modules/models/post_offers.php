27ffe33cc8aff053e9deff28e7a86f611ad25dcd
Wais Khedri <khedriwais@hotmail.com>
2012-11-14 17:33:08 -0500

    post offers form

---
+++ modules/models/post_offers.php
@@ -0,0 +1,10 @@
+<?
+
+?>
+
+<form action = "post.php" method="POST">
+	Description:<br> 
+	<textarea name="description" id="description "rows="6" cols="30"></textarea><br><br>
+	Price: <input type="text" name="price" id="price"><br><br>
+	<input type="submit" value="Submit">
+</form>
