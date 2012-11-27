4fc418f9256bdf39ec7094ab008975512748987c
snwfog <donchoa@gmail.com>
2012-11-26 19:35:01 -0500

    Image upload is up

--- modules/controllers/post.php
+++ modules/controllers/post.php
@@ -14,7 +14,7 @@ class Post_Controller extends Controller implements IRedirectable
 			$data["category_id"] = $_POST["category"];
 			$data["price"] = $_POST["price"];
 			$data["description"] = $_POST["description"];
-			$data["image_url"] = "NULL";
+            $data['image_url'] = $this->handleImage();
 
 			$m_offer = new Offer_Model();
 			$value = array
@@ -37,6 +37,11 @@ class Post_Controller extends Controller implements IRedirectable
 			$m_post = new Post_Model();
 			$post_id = $m_post->setPost($this->getMemberId(), $offer_id);
 
+            // Move image to destination folder
+            if (!move_uploaded_file($_FILES['image']['tmp_name'],
+                IMAGE_PATH . $data["image_url"]))
+                echo "Error upload";
+
 			if ($post_id)
 				$this->redirect("index.php?member");
 		}
@@ -49,4 +54,28 @@ class Post_Controller extends Controller implements IRedirectable
 			$this->display("post.twig", $this->data);
 		}
 	}
+
+    private function handleImage()
+    {
+        $allowedFiletypes = array('jpg', 'png', 'jpeg');
+        $maxFilesize = 524288;
+        $filename = $_FILES['image']['name'];
+
+        if (!empty($filename))
+        {
+            list($name, $ext) = explode(".", $filename);
+
+            if (!in_array($ext, $allowedFiletypes))
+                die('The file you attempted to ulpload is not allowed.');
+            if (filesize($_FILES['image']['tmp_name']) > $maxFilesize)
+                die('The file you attempted to upload is too large.');
+            if (!is_writeable(IMAGE_PATH))
+                die('You cannot upload to the specified directory.');
+
+            $safeName = hash('sha256', $name + date('l jS \of F Y h:i:s A'));
+            return $safeName . "." . $ext;
+        }
+
+        return NULL;
+    }
 }
