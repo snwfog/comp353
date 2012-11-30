8cc20d22a0713156c69ea2db7fe4dfff8fbaec30
snwfog <donchoa@gmail.com>
2012-11-30 15:18:30 -0500

    Database DDL update member delete

--- modules/controllers/admin.php
+++ modules/controllers/admin.php
@@ -11,6 +11,14 @@ class Admin_Controller extends Controller implements IRedirectable
         {
             if (isset($args['member_search']))
                 $this->display('admin-member-search.twig', $this->data);
+            else if (isset($args['buys_and_sells']))
+                $this->display('admin-buys-and-sells.twig', $this->data);
+            else if (isset($args['transactions']))
+                $this->display('admin-transactions.twig', $this->data);
+            else if (isset($args['categories']))
+                $this->display('admin-categories.twig', $this->data);
+            else if (isset($args['regions_and_territories']))
+                $this->display('admin-regions-and-territories.twig', $this->data);
             else
                 $this->display('admin.twig', $this->data);
         }
