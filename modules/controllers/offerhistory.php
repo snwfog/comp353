b8a9df6c67a3c2b2aa9721c972699b05862a3a02
snwfog <donchoa@gmail.com>
2012-12-03 04:14:20 -0500

    Hotfix offer history user id issue

--- modules/controllers/offerhistory.php
+++ modules/controllers/offerhistory.php
@@ -2,14 +2,15 @@
 
 class OfferHistory_Controller extends Controller Implements IRedirectable
 {
-    public function __construct()
+    public function __construct($args)
     {
         parent::__construct();
 
         $m_offers = new Offer_Model();
-
-        $this->data['offers'] = $m_offers->getExpireOfferByMember($this->getMemberId());
-
+		
+		if (isset($args['id']))
+        	$this->data['offers'] = $m_offers->getExpireOfferByMember($args['id']);
+				
         $this->display('offer-history.twig', $this->data);
     }
 }
